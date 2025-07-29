2cc2196b0080c1ee929ad362ca629c6698fcfc89
M Winkler <mwinkler@Ms-MacBook-Pro.local>
2025-07-29 12:09:36 -0500

    fix: use Render-assigned port for deployment

--- server/index.js
+++ server/index.js
@@ -9,5 +9,8 @@ app.use(express.json());
 app.use('/api', winesRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server running on port ${process.env.PORT || 5000}`)
+);
+
   .catch(err => console.error(err));
